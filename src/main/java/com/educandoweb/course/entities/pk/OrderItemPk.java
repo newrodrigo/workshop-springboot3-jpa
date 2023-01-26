595d85183990d79a1bee23acf74d25b13fbc4e43
Rodrigo Oliveira <rodrigocerqueira@outlook.com>
2023-01-26 20:17:14 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/educandoweb/course/entities/pk/OrderItemPk.java
@@ -0,0 +1,65 @@
+package com.educandoweb.course.entities.pk;
+
+import java.io.Serializable;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.Product;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class OrderItemPk implements Serializable {
+	private static final long serialVersionUID = 1L;
+ 
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order oder;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+	
+	public Order getOder() {
+		return oder;
+	}
+	public void setOder(Order oder) {
+		this.oder = oder;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((oder == null) ? 0 : oder.hashCode());
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPk other = (OrderItemPk) obj;
+		if (oder == null) {
+			if (other.oder != null)
+				return false;
+		} else if (!oder.equals(other.oder))
+			return false;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		return true;
+	}
+}
